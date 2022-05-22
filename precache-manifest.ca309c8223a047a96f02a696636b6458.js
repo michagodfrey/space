b593ddf996e428884518d3d8b49d3ad435d5c3ed
Michael Godfrey <m.godfrey59@gmail.com>
2022-05-22 11:35:24 +1000

    Updates

---
+++ precache-manifest.ca309c8223a047a96f02a696636b6458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e4b5acb2d745fd3b053",
+    "url": "/space/static/css/main.5ab72086.chunk.css"
+  },
+  {
+    "revision": "0e4b5acb2d745fd3b053",
+    "url": "/space/static/js/main.e99680d0.chunk.js"
+  },
+  {
+    "revision": "124c6847117f6fd8d13a",
+    "url": "/space/static/js/runtime~main.c48afcec.js"
+  },
+  {
+    "revision": "8e4e6ca6ab2becf2adfc",
+    "url": "/space/static/js/2.3a9c073d.chunk.js"
+  },
+  {
+    "revision": "120f98270447b76b02baaa6f74311b19",
+    "url": "/space/index.html"
+  }
+];
