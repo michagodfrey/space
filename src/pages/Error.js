fb9b4a1bc4b881a10d20c760c0939dc3809b4fcc
Michael Godfrey <m.godfrey59@gmail.com>
2022-09-02 11:39:42 +1200

    changed error page and home route

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Header from "../components/Header";
 import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <div className="destination">
+      <Header />
       <div className="flexbox">
         <div className="flex-item">
-          <h5>oh no, looks like that page is lost in space</h5>
+          <h3>oh no, looks like that page is lost in space</h3>
         </div>
         <div className="flex-item">
-          <Link to="/space">
+          <Link to="/home">
             <span>back home</span>
           </Link>
         </div>
